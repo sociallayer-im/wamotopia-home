a8668761b757d10a7cf3303b038d3e27af6e9112
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-12-09 16:19:49 +0800

    Add hero translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,7 +3,7 @@ export const languages = {
   "zh-cn": "中文",
 }
 
-export const defaultLang = "en"
+export const defaultLang = "zh-cn"
 
 export const showDefaultLang = false
 
@@ -13,6 +13,8 @@ export const ui = {
     "nav.wamotopia": "Wamotopia",
     "nav.city-guide": "City Guide",
     "nav.join": "Join",
+    "hero.learn": "Learn More",
+    "hero.join": "Join",
     "home.introduction": "WAMOTOPIA is a two-week cultural exchange event held in Chiang Mai.",
     "home.sub-introduction":
       "Bringing together the Web3 community and digital nomads, aiming to innovate, collaborate, and shape the future together.",
@@ -22,6 +24,8 @@ export const ui = {
     "nav.wamotopia": "瓦猫托邦",
     "nav.city-guide": "城市指南",
     "nav.join": "加入",
+    "hero.learn": "深入了解",
+    "hero.join": "我想加入",
     "home.introduction": "WAMOTOPIA 是在清迈举办的为期两周的文化交汇活动",
     "home.sub-introduction":
       "融合了 Web3 社区和数字游民，旨在一起创新、协作，并共同塑造未来",
