f4ee671f53a86d389e1b1d00bf7967624df11c27
jiangplus <jiang.plus.times@gmail.com>
2023-12-15 23:18:59 +0700

    update link

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,5 +3,6 @@ export const siteConfig = {
   telegram: "https://t.me/WAMO2023",
   createEvent: 'https://app.sola.day/',
   applyGrantForm: 'https://tally.so/r/wMEqWg',
-  applyToJoinForm: 'https://tally.so/r/3ExrzA'
+  applyToJoinForm: 'https://tally.so/r/3ExrzA',
+  housing: 'https://slender-tarn-59b.notion.site/734f27e840674ca897dc183582960527'
 }
